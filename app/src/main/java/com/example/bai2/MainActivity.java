dc14822444f3110474a9d3acd854b352444fe03f
Thu Phuong <you@example.com>
2019-10-29 11:46:38 +0700

    up

--- app/src/main/java/com/example/bai2/MainActivity.java
+++ app/src/main/java/com/example/bai2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.bai2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,11 +14,13 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button zero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        zero =findViewById(R.id.button_zero);
         mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
@@ -43,5 +48,14 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+        if (mCount!=0){
+            zero.setBackgroundColor(Color.parseColor("#E91E63"));
+        }
+    }
+    //
+    public void zero(View view){
+        mCount=0;
+        mShowCount.setText(Integer.toString(mCount));
+        zero.setBackgroundColor(Color.parseColor("#9E9E9E"));
     }
 }
